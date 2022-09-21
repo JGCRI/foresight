2582855c03b577ad9f1b3706ab016ae93b38aa79
stel561 <tanner.stelmach@pnnl.gov>
2022-09-21 13:26:37 -0400

    Commit the new changes to the ncdf map

--- rshiny/pilot_rshiny_app/ui.R
+++ rshiny/pilot_rshiny_app/ui.R
@@ -18,7 +18,7 @@ library(tidyr)
 steel_trade_data <- read.csv('../../data/pilot_dashboard_data/Iron_Steel_Trade_data_GCAM6.0.csv',skip=1)
 
 shinyUI(fluidPage(
-    
+
     # Application title
     titlePanel("GCAM Steel Trade Balances"),
 
@@ -26,9 +26,9 @@ shinyUI(fluidPage(
     sidebarLayout(
         sidebarPanel(
             sliderInput("year","Year", min=min(steel_trade_data$year),max = max(steel_trade_data$year),1970, sep = "", step=1,
-                        animate = 
+                        animate =
                             animationOptions(interval=300, loop = TRUE)),
-            
+
             selectInput("metric","Metric",
                         c("Production" = "production",
                           "Exports" = "exports_reval",
@@ -36,11 +36,24 @@ shinyUI(fluidPage(
                           "Domestic Supply" = "domestic_supply",
                           "Consumption" = "consumption_reval")),
             ),
-        
+
     # Show a plot of the generated distribution
             mainPanel(
             plotOutput("brandBar"),
-            )
-        )
+            ),
+    output$PrecipMap <- renderPlot({
+        renderPlot
+        precip_dat <- ncvar_get(precip_ncdf_data, input$var)
+        # Get slice of data from date of interest:
+        precip.slice <- precip_dat[,,input$time]
+        # Plot as a heat map:
+        image(lat, long, precip.slice)
+    },
+    height=500,
+    width=800
+    )
+
+        ),
+
     )
 )
