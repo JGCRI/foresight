d3ce0f44384eb56af041e55bcd8175a2c71a7d46
Zarrar Khan <zarrar.khan@pnnl.gov>
2023-04-08 21:51:14 -0400

    Updating query issues

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -21,38 +21,6 @@ const SECOND_IMAGE = {
 };
 
 function Explore() {
-  const [songs, setSongs] = useState([]);
-
-  useEffect(() => {
-    fetchSongs();
-  }, []);
-
-  const fetchSongs = async () => {
-    try {
-      const songData = await API.graphql(graphqlOperation(listSongs));
-      const songList = songData.data.listSongs.items;
-      console.log("song list", songList);
-      setSongs(songList);
-    } catch (error) {
-      console.log("error on fetching songs", error);
-    }
-  };
-
-  const addLike = async idx => {
-    try {
-        const song = songs[idx];
-        song.like = song.like + 1;
-        delete song.createdAt;
-        delete song.updatedAt;
-
-        const songData = await API.graphql(graphqlOperation(updateSong, { input: song }));
-        const songList = [...songs];
-        songList[idx] = songData.data.updateSong;
-        setSongs(songList);
-    } catch (error) {
-        console.log('error on adding Like to song', error);
-    }
-};
 
   return (
     <div className="explore">
@@ -63,21 +31,6 @@ function Explore() {
               firstImage={FIRST_IMAGE}
               secondImage={SECOND_IMAGE}
             />
-          </Row>
-          <Row>
-            {songs.map((song,idx) => {
-              return (
-                <div className="experiment" key={`song${idx}`}>
-                  <p>Song title: {song.title}</p>
-                  <p>Song likes: 
-                    <IconButton aria-label="like" onClick={() => addLike(idx)}>
-                      <FavoriteIcon/>
-                    </IconButton>
-                    {song.like}
-                  </p>
-                </div>
-              );
-            })}
           </Row>
         </Container>
       </div>
