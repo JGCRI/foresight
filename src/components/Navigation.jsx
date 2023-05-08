82dc7723259fe19138bf2bd0b084dbc8a9edb83c
Zarrar Khan <zarrar.khan@pnnl.gov>
2023-05-08 17:11:19 -0400

    Updating icons and nav bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,34 +4,49 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../assets/img/PNNL_CENTER_White.png";
-import {useState} from "react";
+import { FaGlobeAmericas } from "react-icons/fa";
+import { AiFillGithub, AiFillHome, AiFillDashboard, AiFillExperiment } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { RiTeamFill } from "react-icons/ri";
+import { MdHelp } from "react-icons/md";
+
 
 function Navigation() {
 
-  const [expanded, setExpanded] = useState(false);
-  
   return (
-    <Navbar expanded={expanded} fixed="top" bg="dark" variant="dark" expand="lg" className="bartop">
-      <Container>
-        <Navbar.Brand to="/">
-          <img className="logo" src={logo} alt="Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink onClick={() => setExpanded(false)} className="nav-link" to="/">
-              Home
+    <Navbar bg="dark" variant="dark" expand="lg" className="bartop">
+      <Container fluid>
+        <Navbar.Brand href="/"><img className="logo" src={logo} alt="Logo" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto nav-left">
+          <NavLink className="nav-link" to="/">
+              <div className="nav-icon"><AiFillHome /></div>
+              </NavLink>
+            <NavLink className="nav-link" to="/dashboard">
+              <div className="nav-icon"><AiFillDashboard /></div> Dashboard
+              </NavLink>
+              <NavLink className="nav-link" to="/explore">
+              <div className="nav-icon"><AiFillExperiment /></div>Explore
             </NavLink>
-            <NavLink onClick={() => setExpanded(false)} className="nav-link" to="/dashboard">
-              Dashboard
+            <NavLink className="nav-link" to="/world">
+              <div className="nav-icon"><FaGlobeAmericas /></div>World
             </NavLink>
-            <NavLink onClick={() => setExpanded(false)} className="nav-link" to="/explore">
-              Explore
-            </NavLink>
-            <NavLink onClick={() => setExpanded(false)} className="nav-link" to="/world">
-              Globe
-            </NavLink>
-          </Nav>
+            </Nav>
+            <Nav className="ms-auto nav-right">
+              <NavLink className="nav-link" to="/about">
+                <div className="nav-icon"><BsFillInfoCircleFill /></div>About
+              </NavLink>
+              <NavLink className="nav-link" to="/team">
+                <div className="nav-icon"><RiTeamFill /></div>Team
+              </NavLink>
+              <NavLink className="nav-link" to="/help">
+                <div className="nav-icon"><MdHelp /></div>Help
+              </NavLink>
+              <a className="nav-link" href="https://github.com/JGCRI/foresight" target="_blank" rel="noreferrer">
+                <div className="nav-icon"><AiFillGithub /></div>
+              </a>
+            </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
