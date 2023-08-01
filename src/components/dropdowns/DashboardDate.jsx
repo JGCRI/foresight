66e67fc9a83185e8832f336941374225f99dba48
Benjamin <3benknight@gmail.com>
2023-08-01 15:05:41 -0400

    Fixed typo in the DashboardDate css import

--- src/components/dropdowns/DashboardDate.jsx
+++ src/components/dropdowns/DashboardDate.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import '../css/dashboarddate.css';
+import '../css/DashboardDate.css';
 
 export default function DashboardDate({year}) {
     const [startDate, setStartDate] = useState(new Date().setFullYear(year));
