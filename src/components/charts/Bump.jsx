e2754ef326bdda17e7fcaf511a05031fbd1b4983
Benjamin <3benknight@gmail.com>
2023-08-10 15:29:37 -0400

    fixed resizing on graphs

--- src/components/charts/Bump.jsx
+++ src/components/charts/Bump.jsx
@@ -7,147 +7,149 @@ import { ResponsiveBump } from '@nivo/bump'
 // website examples showcase many properties,
 // you'll often use just a few of them.
 const MyResponsiveBump = ({ data }) => (
-    <ResponsiveBump
-        data={data}
-        colors={{ scheme: 'spectral' }}
-        lineWidth={3}
-        activeLineWidth={6}
-        inactiveLineWidth={3}
-        inactiveOpacity={0.15}
-        pointSize={10}
-        activePointSize={12}
-        inactivePointSize={0}
-        pointColor={{ theme: 'background' }}
-        pointBorderWidth={3}
-        activePointBorderWidth={3}
-        pointBorderColor={{ from: 'serie.color' }}
-        axisTop={null}
-        axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: '',
-            legendPosition: 'middle',
-            legendOffset: 32
-        }}
-        axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'ranking',
-            legendPosition: 'middle',
-            legendOffset: -40
-        }}
-        theme={{
-            "text": {
-                "fontSize": 11,
-                "fill": "#333333",
-                "outlineWidth": 0,
-                "outlineColor": "transparent"
-            },
-            "axis": {
-                "domain": {
-                    "line": {
-                        "stroke": "#ffffff",
-                        "strokeWidth": 1
-                    }
-                },
-                "legend": {
-                    "text": {
-                        "fontSize": 12,
-                        "fill": "#ffffff",
-                        "outlineWidth": 0,
-                        "outlineColor": "transparent"
-                    }
-                },
-                "ticks": {
-                    "line": {
-                        "stroke": "#ffffff",
-                        "strokeWidth": 1
-                    },
-                    "text": {
-                        "fontSize": 11,
-                        "fill": "#ffffff",
-                        "outlineWidth": 0,
-                        "outlineColor": "transparent"
-                    }
-                }
-            },
-            "grid": {
-                "line": {
-                    "stroke": "#ffffff",
-                    "strokeWidth": 1
-                }
-            },
-            "legends": {
-                "title": {
-                    "text": {
-                        "fontSize": 11,
-                        "fill": "#333333",
-                        "outlineWidth": 0,
-                        "outlineColor": "transparent"
-                    }
-                },
+    <div className="nivo-wrapper">
+        <ResponsiveBump
+            data={data}
+            colors={{ scheme: 'spectral' }}
+            lineWidth={3}
+            activeLineWidth={6}
+            inactiveLineWidth={3}
+            inactiveOpacity={0.15}
+            pointSize={10}
+            activePointSize={12}
+            inactivePointSize={0}
+            pointColor={{ theme: 'background' }}
+            pointBorderWidth={3}
+            activePointBorderWidth={3}
+            pointBorderColor={{ from: 'serie.color' }}
+            axisTop={null}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: '',
+                legendPosition: 'middle',
+                legendOffset: 32
+            }}
+            axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'ranking',
+                legendPosition: 'middle',
+                legendOffset: -40
+            }}
+            theme={{
                 "text": {
                     "fontSize": 11,
                     "fill": "#333333",
                     "outlineWidth": 0,
                     "outlineColor": "transparent"
                 },
-                "ticks": {
-                    "line": {},
+                "axis": {
+                    "domain": {
+                        "line": {
+                            "stroke": "#ffffff",
+                            "strokeWidth": 1
+                        }
+                    },
+                    "legend": {
+                        "text": {
+                            "fontSize": 12,
+                            "fill": "#ffffff",
+                            "outlineWidth": 0,
+                            "outlineColor": "transparent"
+                        }
+                    },
+                    "ticks": {
+                        "line": {
+                            "stroke": "#ffffff",
+                            "strokeWidth": 1
+                        },
+                        "text": {
+                            "fontSize": 11,
+                            "fill": "#ffffff",
+                            "outlineWidth": 0,
+                            "outlineColor": "transparent"
+                        }
+                    }
+                },
+                "grid": {
+                    "line": {
+                        "stroke": "#ffffff",
+                        "strokeWidth": 1
+                    }
+                },
+                "legends": {
+                    "title": {
+                        "text": {
+                            "fontSize": 11,
+                            "fill": "#333333",
+                            "outlineWidth": 0,
+                            "outlineColor": "transparent"
+                        }
+                    },
                     "text": {
-                        "fontSize": 10,
+                        "fontSize": 11,
                         "fill": "#333333",
                         "outlineWidth": 0,
                         "outlineColor": "transparent"
+                    },
+                    "ticks": {
+                        "line": {},
+                        "text": {
+                            "fontSize": 10,
+                            "fill": "#333333",
+                            "outlineWidth": 0,
+                            "outlineColor": "transparent"
+                        }
                     }
+                },
+                "annotations": {
+                    "text": {
+                        "fontSize": 13,
+                        "fill": "#333333",
+                        "outlineWidth": 2,
+                        "outlineColor": "#ffffff",
+                        "outlineOpacity": 1
+                    },
+                    "link": {
+                        "stroke": "#000000",
+                        "strokeWidth": 1,
+                        "outlineWidth": 2,
+                        "outlineColor": "#ffffff",
+                        "outlineOpacity": 1
+                    },
+                    "outline": {
+                        "stroke": "#000000",
+                        "strokeWidth": 2,
+                        "outlineWidth": 2,
+                        "outlineColor": "#ffffff",
+                        "outlineOpacity": 1
+                    },
+                    "symbol": {
+                        "fill": "#000000",
+                        "outlineWidth": 2,
+                        "outlineColor": "#ffffff",
+                        "outlineOpacity": 1
+                    }
+                },
+                "tooltip": {
+                    "container": {
+                        "background": "#ffffff",
+                        "fontSize": 12
+                    },
+                    "basic": {},
+                    "chip": {},
+                    "table": {},
+                    "tableCell": {},
+                    "tableCellValue": {}
                 }
-            },
-            "annotations": {
-                "text": {
-                    "fontSize": 13,
-                    "fill": "#333333",
-                    "outlineWidth": 2,
-                    "outlineColor": "#ffffff",
-                    "outlineOpacity": 1
-                },
-                "link": {
-                    "stroke": "#000000",
-                    "strokeWidth": 1,
-                    "outlineWidth": 2,
-                    "outlineColor": "#ffffff",
-                    "outlineOpacity": 1
-                },
-                "outline": {
-                    "stroke": "#000000",
-                    "strokeWidth": 2,
-                    "outlineWidth": 2,
-                    "outlineColor": "#ffffff",
-                    "outlineOpacity": 1
-                },
-                "symbol": {
-                    "fill": "#000000",
-                    "outlineWidth": 2,
-                    "outlineColor": "#ffffff",
-                    "outlineOpacity": 1
-                }
-            },
-            "tooltip": {
-                "container": {
-                    "background": "#ffffff",
-                    "fontSize": 12
-                },
-                "basic": {},
-                "chip": {},
-                "table": {},
-                "tableCell": {},
-                "tableCellValue": {}
-            }
-        }}
-        margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
-        axisRight={null}
-    />
+            }}
+            margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
+            axisRight={null}
+        />
+    </div>
 )
 
 export default MyResponsiveBump
