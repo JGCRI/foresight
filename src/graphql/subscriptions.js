284d39f6bd0d8b1055f8511baa11fe225dc4249c
Zarrar Khan <zarrar.khan@pnnl.gov>
2023-09-12 10:20:31 -0400

    Updating graphQL queries after adding in class1 dynamo table

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -199,3 +199,90 @@ export const onDeleteGcamDataTableAggParamRegions = /* GraphQL */ `
     }
   }
 `;
+export const onCreateGcamDataTableAggClass1Regions = /* GraphQL */ `
+  subscription OnCreateGcamDataTableAggClass1Regions(
+    $id: Int
+    $scenario: String
+    $dataset: String
+    $region: String
+    $classLabel: String
+  ) {
+    onCreateGcamDataTableAggClass1Regions(
+      id: $id
+      scenario: $scenario
+      dataset: $dataset
+      region: $region
+      classLabel: $classLabel
+    ) {
+      id
+      scenario
+      dataset
+      region
+      classLabel
+      class
+      x
+      units
+      value
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateGcamDataTableAggClass1Regions = /* GraphQL */ `
+  subscription OnUpdateGcamDataTableAggClass1Regions(
+    $id: Int
+    $scenario: String
+    $dataset: String
+    $region: String
+    $classLabel: String
+  ) {
+    onUpdateGcamDataTableAggClass1Regions(
+      id: $id
+      scenario: $scenario
+      dataset: $dataset
+      region: $region
+      classLabel: $classLabel
+    ) {
+      id
+      scenario
+      dataset
+      region
+      classLabel
+      class
+      x
+      units
+      value
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteGcamDataTableAggClass1Regions = /* GraphQL */ `
+  subscription OnDeleteGcamDataTableAggClass1Regions(
+    $id: Int
+    $scenario: String
+    $dataset: String
+    $region: String
+    $classLabel: String
+  ) {
+    onDeleteGcamDataTableAggClass1Regions(
+      id: $id
+      scenario: $scenario
+      dataset: $dataset
+      region: $region
+      classLabel: $classLabel
+    ) {
+      id
+      scenario
+      dataset
+      region
+      classLabel
+      class
+      x
+      units
+      value
+      createdAt
+      updatedAt
+    }
+  }
+`;
